9ad82b774c3cd00f24ac1b1c04d933a2832d6167
Aba <mhadden-a@hotmail.com>
2021-10-22 21:54:32 +0200

    Populating the flock agents

--- Assets/Game/Scripts/Flock.cs
+++ Assets/Game/Scripts/Flock.cs
@@ -4,10 +4,42 @@ using UnityEngine;
 
 public class Flock : MonoBehaviour
 {
+    public FlockAgent agentPrefab;
+    List<FlockAgent> agents = new List<FlockAgent>();
+    public FlockBehavior behavior;
+
+    [Range(2, 10)]
+    public int startingSize = 5;
+    const float agentDensity = 0.05f;
+
+    [Range(1f, 100f)]
+    public float driveFactor = 2f;
+    [Range(1f, 100f)]
+    public float maxSpeed = 10f;
+    [Range(1f, 10f)]
+    public float neighborRadius = 1.5f;
+    [Range(0f,1f)]
+    public float avoidanceRadiusMultiplier = 0.5f;
+
+    float squareMaxSpeed;
+    float squareNeighborRadius;
+    float squareAvoidanceRadius;
+    public float SquareAvoidanceRadius{ get { return squareAvoidanceRadius; } }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        squareMaxSpeed = maxSpeed * maxSpeed;
+        squareNeighborRadius = neighborRadius * neighborRadius;
+        squareAvoidanceRadius = squareNeighborRadius * avoidanceRadiusMultiplier * avoidanceRadiusMultiplier;
+
+        for (int i = 0; i < startingSize; i++)
+        {
+            FlockAgent newAgent = Instantiate(agentPrefab, Random.insideUnitCircle * startingSize * agentDensity, Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f)), transform);
+            newAgent.name = "Agent " + i;
+            agents.Add(newAgent);
+        }
     }
 
     // Update is called once per frame
