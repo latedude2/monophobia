d9c51a112c49ebfaa882e49094566e4fea550dff
latedude2 <simasc10@gmail.com>
2021-10-23 18:32:46 +0200

    Make humans move forwards and towards center

---
+++ Assets/Game/Scripts/BiasMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiasMovement : MonoBehaviour
+{
+
+    [SerializeField] private float y = 3f;
+    [SerializeField] private float x = 2f;
+    void FixedUpdate()
+    {
+        Vector2 moveDirection = new Vector2(x, y);
+        if(transform.position.y > 0)
+            moveDirection.y = -y;
+        else moveDirection.y = y;
+
+        Debug.Log(moveDirection);
+        GetComponent<Rigidbody2D>().AddForce(moveDirection);
+    }
+}
