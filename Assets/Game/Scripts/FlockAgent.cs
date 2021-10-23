f54ce9ff31449d32f4c72f4e0ef60f0ffde27af5
Aba <mhadden-a@hotmail.com>
2021-10-23 14:43:20 +0200

    Adjusted flocking movement and interactiong Added force to the flocks

--- Assets/Game/Scripts/FlockAgent.cs
+++ Assets/Game/Scripts/FlockAgent.cs
@@ -8,6 +8,8 @@ public class FlockAgent : MonoBehaviour
 
     Collider2D agentCollider;
     public Collider2D AgentCollider { get { return agentCollider; } }
+    
+    private Rigidbody2D rigidBody;
 
 
 
@@ -15,11 +17,14 @@ public class FlockAgent : MonoBehaviour
     void Start()
     {
         agentCollider = GetComponent<Collider2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     public void Move(Vector2 velocity)
     {
         transform.right = velocity;
         transform.position += (Vector3)velocity * Time.deltaTime;
+        var direction = transform.right + transform.position;
+        rigidBody.AddForce(direction * Time.deltaTime);
     }
 }
